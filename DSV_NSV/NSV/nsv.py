2f5b0f57d131c2bca3674846673f67bb8e91e6d3
BulatXaberoff <80531785+BulatXaberoff@users.noreply.github.com>
2022-04-29 16:11:27 +0300

    Update nsv.py

--- DSV_NSV/NSV/nsv.py
+++ DSV_NSV/NSV/nsv.py
@@ -15,6 +15,7 @@ from matplotlib.gridspec import GridSpec
 import print_DSV as dsv
 from sympy import inverse_laplace_transform, exp
 import convertStrFunc as convF
+from matplotlib.ticker import MultipleLocator
 
 check = False
 
@@ -27,7 +28,7 @@ class MyFrame(wx.Frame):
         self.count = 0
         self.panel = wx.Panel(self)
         self.boxS = wx.BoxSizer(wx.VERTICAL)
-
+        self.checkBox = False
         self.panel.SetSizer(self.boxS)
 
         menubar = wx.MenuBar()
@@ -54,11 +55,15 @@ class MyFrame(wx.Frame):
         self.panel.Layout()
 
     def onEx_1(self, event):
-        # while len(self.boxS.GetChildren()) != 1:
-        #     self.boxS.Hide(len(self.boxS.GetChildren()) - 1)
-        #     self.boxS.Remove(len(self.boxS.GetChildren()) - 1)
+        if self.checkBox:
+            print(len(self.boxS.GetChildren()))
+            while len(self.boxS.GetChildren()) != 0:
+                self.boxS.Hide(len(self.boxS.GetChildren()) - 1)
+                self.boxS.Remove(len(self.boxS.GetChildren()) - 1)
+        self.checkBox = True
+
         s = '0,x<=-1&1/2+1/pi*asin(x),-1<=x<=1&1,x>=1'
-        self.res = '$f(x)=\\begin{cases}'
+        self.res = '$Func(x)=\\begin{cases}'
         self.s = 'f_1(x),&x\\vee x_1\\\\f_2(x), & x \\vee x_2\\\\f_3(x), & x \\vee x_3 \\end{cases}$'
         r = ['0,x<=-1&', '1/2+1/pi*asin(x),-1<=x<=1&', '1,x>=1']
         self.count = 3
@@ -74,31 +79,34 @@ class MyFrame(wx.Frame):
             self.bxSizer.Add(txt)
             txt.Label = r[i]
 
-        # self.btm = wx.StaticBitmap(self.panel, -1, wx.Bitmap("prim.png"))
-        # self.boxS.Add(self.btm, 0, wx.ALIGN_CENTER | wx.ALL, 5)
-        # F, f, V = convF.work_input_F(s)
-        # x_1, y_1 = convF.generation_To_arr(F.copy(), V.copy())
-        # x_2, y_2 = convF.generation_To_arr(f.copy(), V.copy())
-        # convF.build_plots(x_1, y_1, f, 'f(x)')
-        # convF.build_plots(x_2, y_2, F, 'F(x)')
-        # x_1,y_1=generation_To_arr(F.copy(), V.copy())
-        # x_2,y_2=generation_To_arr(f.copy(),V.copy())
-        # build_plots(x_1,y_1,F,'F(x)')
-        # build_plots(x_2,y_2,f,'f(x)')
         self.boxS.Layout()
         self.Layout()
         self.panel.Layout()
 
     def onEx_2(self, event):
-        while len(self.boxS.GetChildren()) != 1:
-            self.boxS.Hide(len(self.boxS.GetChildren()) - 1)
-            self.boxS.Remove(len(self.boxS.GetChildren()) - 1)
-        s = r'$0,x<=1&ln(x),1<x<=e&1,x>e$'
-        self.btm = wx.StaticBitmap(self.panel, -1, wx.Bitmap("prim.png"))
-        self.boxS.Add(self.btm, 0, wx.ALIGN_CENTER | wx.ALL, 5)
-        F, f, V = convF.work_input_F(s)
-        x_1, y_1 = convF.generation_To_arr(F.copy(), V.copy())
-        convF.build_plots(x_1, y_1, f, 'f(x)')
+        if self.checkBox:
+            print(len(self.boxS.GetChildren()))
+            while len(self.boxS.GetChildren()) != 0:
+                self.boxS.Hide(len(self.boxS.GetChildren()) - 1)
+                self.boxS.Remove(len(self.boxS.GetChildren()) - 1)
+        self.checkBox = True
+        s = '0,x<-pi/2&1/2*cos(x),-pi/2<x<pi/2&0,x>=pi/2'
+        self.res = '$Func(x)=\\begin{cases}'
+        self.s = 'f_1(x),&x\\vee x_1\\\\f_2(x), & x \\vee x_2\\\\f_3(x), & x \\vee x_3 \\end{cases}$'
+        r = ['0,x<-pi/2&', '1/2*cos(x),-pi/2<x<pi/2&', '0,x>=pi/2']
+        self.count = 3
+        self.CreateNSVDesign()
+        self.Show_Res(self.s)
+        self.boxS.Layout()
+        self.Layout()
+        self.panel.Layout()
+
+        for i in range(self.count):
+            txt = wx.TextCtrl(self.panel, id=i + 1,
+                              name=f"label {i + 1}", style=wx.TE_PROCESS_ENTER)
+            self.bxSizer.Add(txt)
+            txt.Label = r[i]
+
         self.boxS.Layout()
         self.Layout()
         self.panel.Layout()
@@ -116,7 +124,7 @@ class MyFrame(wx.Frame):
         childrens = self.bxSizer.GetChildren()
         s = ""
         info = event.GetString()
-        if info == 'Задать как F(x)':
+        if info == 'Задать как F(x) и найти f(x)':
             for child in childrens:
                 widget = child.GetWindow()
                 s += widget.GetValue()
@@ -126,29 +134,31 @@ class MyFrame(wx.Frame):
             f, F, V = convF.work_input_f(s)
             x_1, y_1 = convF.generation_To_arr(F.copy(), V.copy())
             x_2, y_2 = convF.generation_To_arr(f.copy(), V.copy())
-            convF.build_plots(x_1, y_1, F.copy(), x_2,y_2,f.copy())
-            convF.print_solvingMxDx(f.copy(),V)
+            convF.print_solvingMxDx(f.copy(), V)
             self.boxS.Add(wx.StaticBitmap(self.panel, -1, wx.Bitmap("p.png")),
                           0, wx.ALIGN_CENTER | wx.ALL, 5)
             # convF.build_plots(x_2, y_2, f, 'f(x)')
             self.boxS.Layout()
             self.Layout()
             self.panel.Layout()
-        if info == 'Задать как f(x)':
+            convF.build_plots(x_1, y_1, F.copy(), "F(x)", x_2, y_2, f.copy(), 'f(x)')
+
+        if info == 'Задать как f(x) и найти F(x)':
             for child in childrens:
                 widget = child.GetWindow()
                 s += widget.GetValue()
             convF.work_input_F(s)
             self.btm = wx.StaticBitmap(self.panel, -1, wx.Bitmap("prim.png"))
             self.boxS.Add(self.btm, 0, wx.ALIGN_CENTER | wx.ALL, 5)
-            F,f, V = convF.work_input_F(s)
+            F, f, V = convF.work_input_F(s)
             x_1, y_1 = convF.generation_To_arr(F.copy(), V.copy())
             x_2, y_2 = convF.generation_To_arr(f.copy(), V.copy())
-            convF.build_plots(x_1, y_1, F, 'F(x)')
-            convF.build_plots(x_2, y_2, f, 'f(x)')
+            # convF.build_plots(x_1, y_1, F, 'F(x)')
+            # convF.build_plots(x_2, y_2, f, 'f(x)')
             self.boxS.Layout()
             self.Layout()
             self.panel.Layout()
+            convF.build_plots(x_2, y_2, f.copy(), 'f(x)', x_1, y_1, F.copy(), "F(x)")
 
     def onRemoveWidget(self, event):
         while len(self.boxS.GetChildren()) != 1:
@@ -232,14 +242,14 @@ class MyFrame(wx.Frame):
         self.bxSizer.Layout()
 
     def onDSV(self, event):
-        global check
-        if check:
-            while len(self.gr.GetChildren()) != 0:
-                self.gr.Hide(len(self.gr.GetChildren()))
-                self.gr.Remove(len(self.gr.GetChildren()))
-            check = True
-
+        if self.checkBox:
+            print(len(self.boxS.GetChildren()))
+            while len(self.boxS.GetChildren()) != 0:
+                self.boxS.Hide(len(self.boxS.GetChildren()) - 1)
+                self.boxS.Remove(len(self.boxS.GetChildren()) - 1)
+        self.checkBox = True
         self.gr = wx.GridBagSizer(8, 8)
+        self.boxS.Add(self.gr)
 
         text = wx.StaticText(self.panel, label="Введите кол-во переменных")
         self.gr.Add(text, pos=(0, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=5)
@@ -247,10 +257,13 @@ class MyFrame(wx.Frame):
         txctrl = wx.TextCtrl(self.panel, style=wx.TE_PROCESS_ENTER)
         self.gr.Add(txctrl, pos=(1, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=5)
 
-        self.figure = matplotlib.figure.Figure()
+        self.figure = matplotlib.figure.Figure(figsize=(8, 4))
+        # self.axes1 = self.figure.add_subplot()
         self.gs = self.figure.add_gridspec(2, 2)
         self.axes = self.figure.add_subplot(self.gs[:, 0])
         self.axes1 = self.figure.add_subplot(self.gs[:, 1])
+        # self.build_canvas1()
+        # self.build_canvas2()
 
         self.canvas = FigureCanvasWxAgg(self.panel, -1, self.figure)
 
@@ -264,20 +277,69 @@ class MyFrame(wx.Frame):
 
         self.Bind(wx.EVT_TEXT_ENTER, self.onParam, txctrl)
         self.Bind(wx.EVT_TEXT_ENTER, self.onGrid, self.grid)
-        self.panel.SetSizer(self.gr)
+        self.panel.SetSizer(self.boxS)
         self.panel.Layout()
 
+    def pos_arr(self, _x, first_el, last_el):
+        m1 = [first_el]
+        for e in _x:
+            m1.append(e)
+        m1.append(last_el)
+        return m1
+
+    def build_canvas1(self, arr_p, x, y):
+        self.axes.clear()
+        self.axes.clear()
+        axes1 = np.linspace(-100, 100)
+        axes2 = [0] * len(axes1)
+        ax = self.axes
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.plot(axes1, axes2, color='k')
+        ax.plot(axes2, axes1, color='k')
+        x_ = x.copy()
+        y_ = y.copy()
+        x_ = self.pos_arr(x_, min(x) - 10, max(x) + 10)
+        y_ = self.pos_arr(y_, min(y), max(y))
+        ax.step(x_, y_, 'r--o', label='F(x)')
+        ax.legend()
+        ax.grid()
+        ax.set_ylim(-0.1, 1.2)
+        ax.set_xlim(min(x) - 2, max(x) + 2)
+        self.axes = ax
+        self.canvas.draw()
+
+    def build_canvas2(self, arr_p, x, y):
+        self.axes1.clear()
+        self.axes1.clear()
+        axes1 = np.linspace(-100, 100)
+        axes2 = [0] * len(axes1)
+        ax1 = self.axes1
+        # ax1.xaxis.set_major_locator(MultipleLocator(base=1))
+        ax1.set_xlabel('X')
+        ax1.plot(axes1, axes2, color='k')
+        ax1.plot(axes2, axes1, color='k')
+        ax1.plot(self.arr_x, arr_p, 'b--o', label='f(x)')
+        ax1.legend()
+        ax1.grid()
+        ax1.set_ylim(-0.1, 1.2)
+        ax1.set_xlim(min(x) - 2, max(x) + 2)
+        self.axes1 = ax1
+        self.canvas.draw()
+
     def onNSV(self, event):
+        if self.checkBox:
+            while len(self.boxS.GetChildren()) != 0:
+                self.boxS.Hide(len(self.boxS.GetChildren()) - 1)
+                self.boxS.Remove(len(self.boxS.GetChildren()) - 1)
+        self.checkBox = True
         global check
         if check:
-            # while len(self.gr.GetChildren()) != 0:
-            #     self.gr.Hide(len(self.gr.GetChildren()))
-            #     self.gr.Remove(len(self.gr.GetChildren()))
             self.panel.DestroyChildren()
             check = True
         # self.panel=wx.Panel(self)
         self.CreateNSVDesign()
-        self.res = r"$f(x)=\begin{cases}"
+        self.res = r"$Func(x)=\begin{cases}"
         self.s = \
             rf"f_{self.count + 1}(x),&x\vee x_{self.count + 1} " \
             r"\end{cases}$"
@@ -296,7 +358,7 @@ class MyFrame(wx.Frame):
         self.sb = wx.StaticBitmap(self.panel, -1, wx.Bitmap("output.png"))
         self.sb.Hide()
         self.grb.Add(self.sb, pos=(0, 0), span=(2, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=5)
-        m_comboBox2Choices = ["Задать как F(x)", "Задать как f(x)"]
+        m_comboBox2Choices = ["Задать как F(x) и найти f(x)", "Задать как f(x) и найти F(x)"]
         self.m_comboBox2 = wx.ComboBox(self.panel, wx.ID_ANY, "Combo!", wx.DefaultPosition, wx.DefaultSize,
                                        m_comboBox2Choices, 0)
         self.grb.Add(self.m_comboBox2, pos=(2, 2), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=5)
@@ -312,37 +374,38 @@ class MyFrame(wx.Frame):
     def onGrid(self, event):
         print("!")
 
-    def _drawGraph(self, xvals, yvals):
-        # !!!
-        # Удалим предыдущий график, если он есть
-        self.axes.clear()
+    def build_plot(self, arr_p, x, y):
+        axes1 = np.linspace(-100, 100)
+        axes2 = [0] * len(axes1)
 
-        self.axes.clear()
-        # Нарисуем новый график
-        self.axes.plot(xvals, yvals)
+        fig = plt.figure(figsize=(15, 6))
+        ax = fig.add_subplot()
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
 
-        # Включим сетку
-        self.axes.grid()
+        ax1 = plt.subplot(1, 2, 1)
+        plt.plot(axes1, axes2, color='k')
+        plt.plot(axes2, axes1, color='k')
+        x_ = x.copy()
+        y_ = y.copy()
+        x_ = self.pos_arr(x_, min(x) - 100, max(x) + 100)
+        y_ = self.pos_arr(y_, min(y), max(y))
+        ax1.step(x_, y_, 'r--o', linewidth=3, label='F(x)')
+        ax1.legend()
+        ax1.grid()
+        ax1.set_ylim(-0.1, 1.2)
+        ax1.set_xlim(min(x) - 5, max(x) + 5)
 
-        # self.axes.legend([u"Gaussian"])
+        ax2 = plt.subplot(1, 2, 2)
+        plt.plot(axes1, axes2, color='k')
+        plt.plot(axes2, axes1, color='k')
+        ax2.plot(self.arr_x, arr_p, 'b--o', linewidth=3, label='f(x)')
+        ax2.legend()
+        ax2.grid()
+        ax2.set_ylim(-0.1, 1.2)
+        ax2.set_xlim(min(self.arr_x) - 5, max(self.arr_x) + 5)
 
-        self.canvas.draw()
-
-    def _drawGraph1(self, xvals, yvals):
-        # !!!
-        # Удалим предыдущий график, если он есть
-        self.axes1.clear()
-
-        self.axes1.clear()
-        # Нарисуем новый график
-        self.axes1.plot(xvals, yvals)
-
-        # Включим сетку
-        self.axes1.grid()
-
-        self.axes1.legend([u"Gaussian"])
-
-        self.canvas.draw()
+        plt.show()
 
     def onParam(self, event):
         num = int(event.GetString())
@@ -351,7 +414,6 @@ class MyFrame(wx.Frame):
             self.gr.Hide(len(self.gr.GetChildren()) - 1)
             self.gr.Remove(len(self.gr.GetChildren()) - 1)
 
-        self.grid.Destroy()
         self.grid = wx.grid.Grid(self.panel)
         self.grid.CreateGrid(2, num)
         self.grid.SetRowLabelValue(0, "x_i")
@@ -368,8 +430,10 @@ class MyFrame(wx.Frame):
         but2.Label = "Вычислить"
         self.gr.Add(but2, pos=(4, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=5)
 
-        self.panel.SetSizer(self.gr)
-        self.panel.Layout()
+        # self.boxS.Add(self.gr)
+        # self.panel.SetSizer(self.boxS)
+        self.boxS.Layout()
+        # self.panel.Layout()
         self.Bind(wx.EVT_BUTTON, self.onBut1, but1)
         self.Bind(wx.EVT_BUTTON, self.onBut2, but2)
 
@@ -412,20 +476,17 @@ class MyFrame(wx.Frame):
             return
 
         x, p = self.arr_x, arr_p
-        # M_X = dsv.M_X(x, p)[1]
-        # D_X = dsv.D_X(x, p)[1]
-        # Std = dsv.S_X(x, p)[1]
-        M_X = 1
-        D_X = 2
-        Std = 3
+        M_X = dsv.M_X(x, p)[1]
+        D_X = dsv.D_X(x, p)[1]
+        Std = dsv.S_X(x, p)[1]
+
         stattxt = wx.StaticText(self.panel)
         stattxt.LabelText = f"{M_X}-мат.ожидание\n{D_X}-дисперсия\n{Std}-стандартное отклонение"
-        self._drawGraph(self.arr_x, arr_p)
 
-        # bmp = wx.Bitmap("why.jpg")
-        # bmp = self.scale_bitmap(bmp, 40, 40)
+        bmp = wx.Bitmap("why.jpg")
+        bmp = self.scale_bitmap(bmp, 40, 40)
         stb = wx.Button(self.panel, size=(50, 50), name="button")
-        # stb.SetBitmap(bmp)
+        stb.SetBitmap(bmp)
 
         arr_F = list()
         arr_F.append(0)
@@ -440,26 +501,11 @@ class MyFrame(wx.Frame):
         x = self.arr_x.copy()
         y = arr_F.copy()
         x.append(x[-1] + 5)
-        # f_x = dsv.F_X(x, p)[0]
-        self._drawGraph1(x, x)
-        fig = plt.figure(figsize=(15, 6))
-        gs = GridSpec(ncols=2, nrows=2, figure=fig)
-        ax1 = plt.subplot(gs[:, 0])
-        ax1.step(x, y, 'r--o', linewidth=3)
-        ax1.grid()
-        ax1.spines['left'].set_position('center')
-        ax1.spines['bottom'].set_position('center')
-        ax1.spines['top'].set_visible(False)
-        ax1.spines['right'].set_visible(False)
-
-        ax2 = fig.add_subplot(gs[:, 1])
-        ax2.plot(self.arr_x, arr_p, 'b--o', linewidth=3)
-        ax2.grid()
-        ax2.spines['left'].set_position('center')
-        ax2.spines['bottom'].set_position('center')
-        ax2.spines['top'].set_visible(False)
-        ax2.spines['right'].set_visible(False)
-        fig.show()
+        f_x = dsv.F_X(x, p)[0]
+        # self._drawGraph(self.arr_x, arr_p)
+        # self._drawGraph1(f_x, x)
+        self.build_canvas1(arr_p, x, y)
+        self.build_canvas2(arr_p, x, y)
 
         img = wx.Image("output1.png", type=wx.BITMAP_TYPE_ANY, index=-1)
         sb = wx.StaticBitmap(self.panel, -1, wx.Bitmap(img))
@@ -470,6 +516,7 @@ class MyFrame(wx.Frame):
 
         self.gr.Layout()
         self.Bind(wx.EVT_BUTTON, self.onWhyButt, stb)
+        self.build_plot(arr_p, x, y)
 
     def __del__(self):
         pass
@@ -512,5 +559,3 @@ app = wx.App()
 wnd = MyFrame(None)
 wnd.Show()
 app.MainLoop()
-
-#
